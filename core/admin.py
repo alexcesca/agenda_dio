9447fd1824520d3a572045949af076441367c71d
Alex Cesca <alexcesca@gmail.com>
2021-04-03 03:20:15 -0300

    criado rotas e templates

--- core/admin.py
+++ core/admin.py
@@ -4,5 +4,6 @@ from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo','data_evento','data_criacao')
+    list_filter = ('titulo','data_evento','usuario',)
 
 admin.site.register(Evento,EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -16,3 +16,6 @@ class Evento(models.Model):
     # esta função retorna o titulo para apresentação no django admin
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M')
