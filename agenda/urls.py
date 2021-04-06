b6b06a7440f61f096ab902bf928d196570ba7eaf
Alex Cesca <alexcesca@gmail.com>
2021-04-06 01:48:20 -0300

    Incluido autenticação de usuario

--- agenda/urls.py
+++ agenda/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agenda/',views.lista_eventos)
+    path('agenda/',views.lista_eventos),
+    path('',RedirectView.as_view(url="/agenda/")),
+    path('login/',views.login_user),
+    # quando for post não colocar a barra no final
+    path('login/submit',views.submit_login),
+    path('logout',views.logout_user)
 ]
